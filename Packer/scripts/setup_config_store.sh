3ce50c315400a506083b57b5f29124a5fba2a197
Roy Bahian <rbahian@hubcitymedia.com>
2018-09-06 18:01:05 -0400

    packer build for specific ds store

--- Packer/scripts/setup_config_store.sh
+++ Packer/scripts/setup_config_store.sh
@@ -5,6 +5,8 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 #This code runs in VM part of the packer build
+SERVICE_USER=opendj
+SERVICE_GROUP=forgerock
 ZIP_FILE=cfg.zip
 OPENDJ_HOME=/opt/opendj/
 FILE_HOME=/opt/bits
@@ -13,12 +15,10 @@ cd ${FILE_HOME}
 sudo apt-get update 
 sudo unzip /home/ubuntu/${ZIP_FILE} .
 sudo chown ${SERVICE_USER}:${SERVICE_GROUP} -R ${FILE_HOME}
-sudo rm /home/ubuntu/${ZIP_FILE}
+#sudo rm /home/ubuntu/${ZIP_FILE}
 
-sudo su - opendj
-cd cfg/Artifacts
+sudo su opendj
+cd ${FILE_HOME}cfg/Artifacts
 ./setup_opendj.sh
-
-
 
 echo "Config Store Configured!"
